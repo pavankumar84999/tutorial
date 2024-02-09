847ed94411b3fd3304ea72963b84c62bdb0f75ef
pavan <pk7708310@gmail.com>
2024-02-09 03:51:39 +0000

    solution

---
+++ src/main/java/com/example/nxtstayz/service/RoomJpaService.java
@@ -0,0 +1,116 @@
+/*
+ *
+ * You can use the following import statements
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.http.HttpStatus;
+ * import org.springframework.stereotype.Service;
+ * import org.springframework.web.server.ResponseStatusException;
+ * import java.util.ArrayList;
+ * import java.util.List;
+ * 
+ */
+
+// Write your code here 
+
+package com.example.nxtstayz.service;
+
+import com.example.nxtstayz.model.Room;
+import com.example.nxtstayz.model.Hotel;
+
+import com.example.nxtstayz.repository.RoomRepository;
+import com.example.nxtstayz.repository.RoomJpaRepository;
+import com.example.nxtstayz.repository.HotelJpaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+
+public class RoomJpaService implements RoomRepository {
+
+    @Autowired
+    private RoomJpaRepository roomJpaRepository;
+
+    @Autowired
+    private HotelJpaRepository hotelJpaRepository;
+
+    @Override
+    public ArrayList<Room> getRooms() {
+        List<Room> roomList = roomJpaRepository.findAll();
+        ArrayList<Room> rooms = new ArrayList<>(roomList);
+        return rooms;
+    }
+
+    @Override
+    public Room getRoomById(int roomId) {
+        try {
+            return roomJpaRepository.findById(roomId).get();
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Room addRoom(Room room) {
+        try {
+            int roomId = room.getHotel().getHotelId();
+            Hotel hotel = hotelJpaRepository.findById(hotelId).get();
+            room.setHotel(hotel);
+            return roomJpaRepository.save(room);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+    @Override
+    public Room updateRoom(int roomId, Room room) {
+        try {
+            Room newRoom = roomJpaRepository.findById(roomId).get();
+            if (room.getRoomName() != null) {
+                newRoom.setRoomName(room.getRoomName());
+            }
+            if (room.getType() != null) {
+                newRoom.setType(room.getType());
+            }
+
+            if (room.getPrice() != null) {
+                newRoom.setPrice(room.getPrice());
+            }
+
+            if (room.getHotel() != null) {
+                int hotelId = room.getHotel().getHotelId();
+                Hotel hotel = hotelJpaRepository.findById(hotelId).get();
+                room.setHotel(hotel);
+            }
+            return roomJpaRepository.save(newRoom);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void deleteRoom(int roomId) {
+        try {
+            roomJpaRepository.deleteById(roomId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+    }
+
+    @Override
+
+    public Hotel getRoomHotel(int roomId) {
+        try {
+            Room room = roomJpaRepository.findById(roomId).get();
+            return room.getHotel();
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
